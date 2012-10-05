54918c6261b89f3407036752030ade4bf0fc1507
Marcio Lima <mdlima.github@mailnull.com>
2012-10-05 14:04:03 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petcuida::Application.config.secret_token = '101034cd5244b4b4e34458122a7e13ca27daca82c270eba88b0414f727e7051d7e8b4fb19f1a742b9980c110abd99808cc557007efdd80364c0e3af8ebff1d71'
