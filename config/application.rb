8792d6d3f9e135c71884208e4789311d8ebc040b
Marcio Lima <mdlima.github@mailnull.com>
2012-10-11 19:20:07 -0300

    Set up time zone to Brasilia.

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Petcuida
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -77,5 +77,6 @@ module Petcuida
 
     # Recommended for Heroku deployment, partially load the application
     config.assets.initialize_on_precompile = false
+    
   end
 end
