e5ac11f06ab26c39f113641971fa0d22f9f9e538
Marcio Lima <mdlima.github@mailnull.com>
2012-10-16 01:32:07 -0300

    Set up Mailchimp integration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
@@ -55,7 +57,7 @@ class User < ActiveRecord::Base
   def add_user_to_mailchimp
     unless self.email.include?('@test.com') or !self.opt_in?
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
-      list_id = mailchimp.find_list_id_by_name "visitors"
+      list_id = mailchimp.find_list_id_by_name "Pet Cuida - Lançamento"
       info = { }
       result = mailchimp.list_subscribe(list_id, self.email, info, 'html', false, true, false, true)
       Rails.logger.info("MAILCHIMP SUBSCRIBE: result #{result.inspect} for #{self.email}")
@@ -65,8 +67,8 @@ class User < ActiveRecord::Base
   def remove_user_from_mailchimp
     unless self.email.include?('@test.com')
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
-      list_id = mailchimp.find_list_id_by_name "visitors"
-      result = mailchimp.list_unsubscribe(list_id, self.email, true, false, true)  
+      list_id = mailchimp.find_list_id_by_name "Pet Cuida - Lançamento"
+      result = mailchimp.list_unsubscribe(list_id, self.email, false, false, true)  
       Rails.logger.info("MAILCHIMP UNSUBSCRIBE: result #{result.inspect} for #{self.email}")
     end
   end
