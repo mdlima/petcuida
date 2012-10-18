1e9f0836a8f4a4b80d20d80a9060a3dbbf8f6c33
Marcio Lima <mdlima.github@mailnull.com>
2012-10-18 17:25:40 -0300

    Add /thanks path and view.

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Petcuida::Application.routes.draw do
   devise_scope :user do
     root :to => "devise/registrations#new"
     # match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    get 'thanks' => 'devise/registrations#thanks'
   end
 
   devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
-  end
   # 
   # match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
   # 
+  # resources :users, :only => [:show, :index] do
   #   get 'invite', :on => :member
   # end
 end
