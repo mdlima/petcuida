e951db7f13ce9c59c47d57ea3499ae2f5ceb09ec
Marcio Lima <mdlima.github@mailnull.com>
2012-11-06 17:17:24 -0200

    Create vets factory.

---
+++ spec/factories/vets.rb
@@ -0,0 +1,14 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :vet do
+    name 'Test Vet'
+    email 'example_vet@example.com'
+    password 'please'
+    password_confirmation 'please'
+    phone '11999990000'
+    zip_code '04100-000'
+    # required if the Devise Confirmable module is used
+    confirmed_at Time.now
+  end
+end
