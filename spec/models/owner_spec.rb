17b47e692499b5a9d661403a1852738f24050f51
Marcio Lima <mdlima.github@mailnull.com>
2012-11-09 02:00:40 -0200

    Remove pending message from owner integration tests.

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Owner do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Nothing especific to test for yet.
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
