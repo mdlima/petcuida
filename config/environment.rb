4c779de3789f18456959f92112a109027e9d959f
Marcio Lima <mdlima.github@mailnull.com>
2012-11-09 01:54:02 -0200

    Config Haml for ugly output (same as in production).

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 Haml::Template.options[:format] = :html5
+Haml::Template.options[:ugly] = true
 
 # Initialize the rails application
 Petcuida::Application.initialize!
