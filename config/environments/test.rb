7c98ada53ed31908f1c0b0584d469c8af242628f
Marcio Lima <mdlima.github@mailnull.com>
2012-11-10 01:11:42 -0200

    Add config to easily enable verbose output and ruby warnings.
    This is useful to verify compliance with Ruby style guide
    https://github.com/bbatsov/ruby-style-guide/.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,7 @@ Petcuida::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=3600"
+  config.static_cache_control = 'public, max-age=3600'
 
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
@@ -37,5 +37,8 @@ Petcuida::Application.configure do
   
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'example.com' }
+  
+  # Enable Ruby warnings
+  # $VERBOSE = true
 
 end
