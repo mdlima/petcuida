02b0c4753958c6f42269334216e44860b8e2c9ff
Marcio Lima <mdlima.github@mailnull.com>
2012-11-12 02:52:13 -0200

    Apply ruby style guidelines.

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,12 +55,12 @@ class User < ActiveRecord::Base
     p = {}
     p[:password] = params[:password]
     p[:password_confirmation] = params[:password_confirmation]
-    update_attributes(p)
+    update_attributes p
   end
   
   # new function to determine whether a password has been set
   def has_no_password?
-    self.encrypted_password.blank?
+    encrypted_password.blank?
   end
 
   # new function to provide access to protected method pending_any_confirmation
@@ -82,7 +82,7 @@ class User < ActiveRecord::Base
   NAME_CAPS_EXCEPTIONS = %w[da das de des di dis do dos du dus e]
   
   def init
-    self.opt_in = true if (self.has_attribute? :opt_in) && self.opt_in.nil?
+    self.opt_in = true if has_attribute?(:opt_in) && opt_in.nil?
   end
   
   def format_name (nm)
@@ -93,75 +93,72 @@ class User < ActiveRecord::Base
   end
   
   def format_fields
-    unless self.phone.blank?
-      phone_parts = self.phone.gsub(/^[0\D]+|[\D]+/,'').match(/(\d{2})(\d{4,5})(\d{4})/)
+    unless phone.blank?
+      phone_parts = phone.gsub(/^[0\D]+|[\D]+/,'').match(/(\d{2})(\d{4,5})(\d{4})/)
       # => #<MatchData "11999991234" 1:"11" 2:"99999" 3:"1234">
       self.phone = "0#{phone_parts[1]}-#{phone_parts[2]}-#{phone_parts[3]}"
     end
     
-    unless self.zip_code.blank?
-      zip_parts = self.zip_code.gsub(/\D/,'').match(/(\d{5})(\d{3})/)
+    unless zip_code.blank?
+      zip_parts = zip_code.gsub(/\D/,'').match(/(\d{5})(\d{3})/)
       # => #<MatchData "12345012" 1:"12345" 2:"012">
       self.zip_code = "#{zip_parts[1]}-#{zip_parts[2]}"
     end
   end
   
   def phone_number_validation
-    unless self.phone.blank?
+    unless phone.blank?
       # Keeps just numbers with no leading zeroes
-      phone_number = self.phone.gsub(/^[0\D]+|[\D]+/,'')
+      phone_number = phone.gsub(/^[0\D]+|[\D]+/,'')
       errors.add(:phone,"é muito curto, lembre-se de colocar o DDD.") if phone_number.length < 10
       errors.add(:phone,"é muito longo, utilize o formato 011-99999-9999.") if phone_number.length > 11
     end
   end
 
   def add_user_to_mailchimp
-    unless self.email.include?('@test.com') # or !self.opt_in?
+    unless email.include?('@test.com') # or !self.opt_in?
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
       list_id = Rails.env.production? ? "8b1d8c9a12" : "4368979616" # mailchimp.find_list_id_by_name "Pet Cuida - Lançamento"
-      info = { 
-        "SOURCE" => "website", 
-        "USER_TYPE" => self.type, 
-        "TYPE_ID" => (self.type == "Owner") ? 1 : 2,
-        "OPTIN_IP" => self.opt_in_ip
-      }
-      info["FNAME"]    = self.name      if self.name
-      info["LNAME"]    = self.last_name if self.last_name
-      info["PHONE"]    = self.phone     if self.phone
-      info["ZIP_CODE"] = self.zip_code  if self.zip_code
-      result = mailchimp.list_subscribe(list_id, self.email, info, 'html', false, true, false, false)
-      Rails.logger.info("MAILCHIMP SUBSCRIBE: result #{result.inspect} for #{self.inspect}")
+      info = { "SOURCE" => "website",
+               "USER_TYPE" => type,
+               "TYPE_ID" => (type == "Vet") ? 2 : 1,
+               "OPTIN_IP" => opt_in_ip }
+               
+      info["FNAME"]    = name      if name
+      info["LNAME"]    = last_name if last_name
+      info["PHONE"]    = phone     if phone
+      info["ZIP_CODE"] = zip_code  if zip_code
+      result = mailchimp.list_subscribe(list_id, email, info, 'html', false, true, false, false)
+      Rails.logger.info "MAILCHIMP SUBSCRIBE: result #{result.inspect} for #{inspect}"
     end
   end
   
   def remove_user_from_mailchimp
-    unless self.email.include?('@test.com')
+    unless email.include?('@test.com')
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
       list_id = Rails.env.production? ? "8b1d8c9a12" : "4368979616" # mailchimp.find_list_id_by_name "Pet Cuida - Lançamento"
-      result = mailchimp.list_unsubscribe(list_id, self.email, false, true, true)  
-      Rails.logger.info("MAILCHIMP UNSUBSCRIBE: result #{result.inspect} for #{self.email}")
+      result = mailchimp.list_unsubscribe(list_id, email, false, true, true)  
+      Rails.logger.info "MAILCHIMP UNSUBSCRIBE: result #{result.inspect} for #{email}"
     end
   end
   
   def send_welcome_email
-    unless self.email.include?('@test.com')
+    unless email.include?('@test.com')
       mandrill = Mandrill::API.new(ENV["MANDRILL_API_KEY"])
-      result = mandrill.messages 'send-template', { 
-        :template_name => "Pet Cuida - User Registration Welcome#{Rails.env.production? ? "" : " Dev"}", 
-        :template_content => [{ :name => "subject", :content => "Confirmação de Cadastro - Pet Cuida" }], 
-        :message => {
-          :subject    => "Confirmação de Cadastro - Pet Cuida", 
-          :from_email => "atendimento@petcuida.com.br", 
-          :from_name  => "Pet Cuida", 
-          :to         => [{ :email => self.email, :name => self.name }], 
-          :headers    => { "X-MC-GoogleAnalytics" => "www.petcuida.com.br", "X-MC-Tags" => "user-registration-welcome" }, 
-          :global_merge_vars => [
-            { :name => "SUBJECT", :content => "Confirmação de Cadastro - Pet Cuida" }, 
-            { :name => "CURRENT_YEAR", :content => Date.today.year }], 
-          :google_analytics_domains => ["www.petcuida.com.br"], 
-          :google_analytics_campaign => "welcome_email" }, 
-          :async => false }
-      Rails.logger.info("MANDRILL WELCOME EMAIL: result #{result.inspect} for #{self.email}")
+      result = mandrill.messages 'send-template', 
+                                { :template_name => "Pet Cuida - User Registration Welcome#{Rails.env.production? ? "" : " Dev"}", 
+                                  :template_content => [{ :name => "subject", :content => "Confirmação de Cadastro - Pet Cuida" }], 
+                                  :message => { :subject    => "Confirmação de Cadastro - Pet Cuida", 
+                                                :from_email => "atendimento@petcuida.com.br", 
+                                                :from_name  => "Pet Cuida", 
+                                                :to         => [{ :email => email, :name => name }], 
+                                                :headers    => { "X-MC-GoogleAnalytics" => "www.petcuida.com.br", "X-MC-Tags" => "user-registration-welcome" }, 
+                                                :global_merge_vars => [ { :name => "SUBJECT", :content => "Confirmação de Cadastro - Pet Cuida" }, 
+                                                                        { :name => "CURRENT_YEAR", :content => Date.today.year } ], 
+                                                :google_analytics_domains => ["www.petcuida.com.br"], 
+                                                :google_analytics_campaign => "welcome_email" }, 
+                                                :async => false }
+      Rails.logger.info "MANDRILL WELCOME EMAIL: result #{result.inspect} for #{email}"
     end
   end
 
